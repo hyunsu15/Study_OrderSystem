990d894799ea423a815a4974aac20d3d9bfcd6d7
kim hyun jin <jdff07@naver.com>
2025-05-19 13:53:58 +0900

    MenuStatistics 오류 수정

--- src/main/java/order/MenuStatistics.java
+++ src/main/java/order/MenuStatistics.java
@@ -3,6 +3,7 @@ package main.java.order;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class MenuStatistics {
     private final OrderManager orders;
@@ -12,16 +13,18 @@ public class MenuStatistics {
     }
 
     public List<Map.Entry<MenuItem, Integer>> getTopN(int n) {
-        return orders.getOrderCounts().entrySet().stream()
-                .sorted(Map.Entry.<MenuItem, Integer>comparingByValue(Comparator.reverseOrder()))
+        return orders.getOrderCounts()
+                .entrySet()
+                .stream()
+                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
                 .limit(n)
-                .toList();
+                .collect(Collectors.toList());
     }
 
     public List<Map.Entry<MenuItem, Integer>> getBottomN(int n) {
         return orders.getOrderCounts().entrySet().stream()
                 .sorted(Map.Entry.comparingByValue())
                 .limit(n)
-                .toList();
+                .collect(Collectors.toList());
     }
 }

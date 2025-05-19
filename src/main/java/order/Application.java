dab5b20f8a793efa6a76fb6497198bd2913356fd
kim hyun jin <jdff07@naver.com>
2025-05-19 14:04:41 +0900

    Application 완성

--- src/main/java/order/Application.java
+++ src/main/java/order/Application.java
@@ -2,6 +2,40 @@ package main.java.order;
 
 public class Application {
   public static void main(String[] args) {
-    // TODO: 프로그램 구현
+    Reader reader = new Reader();
+    Printer printer = new Printer();
+    Menu menu = new Menu();
+    OrderManager orderManager = new OrderManager();
+    SalesCalculator salesCalculator = new SalesCalculator();
+    MenuStatistics menuStatistics = new MenuStatistics(orderManager);
+
+    while (true) {
+      printer.printMainMenu();
+      int choice = reader.readInt();
+      switch (choice) {
+        case 1:
+          printer.printMenu(menu.getItems());
+          break;
+        case 2:
+          printer.printPopularMenu(
+                  menuStatistics.getTopN(3)
+          );
+          break;
+        case 3:
+          printer.printUnpopularMenu(
+                  menuStatistics.getBottomN(3)
+          );
+          break;
+        case 4:
+          int total = salesCalculator.calculate(menu, orderManager);
+          printer.printSales(total);
+          break;
+        case 5:
+          printer.printExit();
+          return;
+        default:
+          printer.printInvalidInput();
+      }
+    }
   }
 }
